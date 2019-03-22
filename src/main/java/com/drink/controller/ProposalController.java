5d899eb72564c9c7c587d68730945ec5dbf100ad
sh.an <sh.an@isecommerce.co.kr>
2019-03-22 15:33:19 +0900

    commit

--- src/main/java/com/drink/controller/ProposalController.java
+++ src/main/java/com/drink/controller/ProposalController.java
@@ -128,7 +128,6 @@ public class ProposalController {
 		List<DataMap> rtnMap = brandService.BrandList(rtMap);
 		List<DataMap> mBrandCd = brandService.BrandMCdList(rtMap);
 		
-		mav.addObject("prps_id", rtMap.getString("prps_id"));
 		mav.addObject("brandList", rtnMap);
 		mav.addObject("mBrandCd", mBrandCd);
 		mav.setViewName("proposal/proposalform2");
